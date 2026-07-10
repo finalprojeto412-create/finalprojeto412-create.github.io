d29e6dd92af1cfacfdcae5c4d40681cb0ca81929
finalprojeto412-create <finalprojeto412@gmail.com>
2026-07-09 21:26:17 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://nnqajazhftanluuouihh.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_PLtLUitzi7OyYarqL1sjAg_wFYRMafm";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucWFqYXpoZnRhbmx1dW91aWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1ODQxNTYsImV4cCI6MjA5OTE2MDE1Nn0.PoRQ65eOy1z2dzDW96LALev3jGymj4lg2TJFMDl1fzU";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
