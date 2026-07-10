8500c179eb5f74333e5ce430698818dfbf93f2c8
finalprojeto412-create <finalprojeto412@gmail.com>
2026-07-09 21:28:16 -0300

    Rename supabase variable to supabaseClient

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
 const SUPABASE_URL = "https://nnqajazhftanluuouihh.supabase.co";
-
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucWFqYXpoZnRhbmx1dW91aWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1ODQxNTYsImV4cCI6MjA5OTE2MDE1Nn0.PoRQ65eOy1z2dzDW96LALev3jGymj4lg2TJFMDl1fzU";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
-// TESTE
+
 console.log("Supabase conectado!");
-console.log(supabase);
+console.log(supabaseClient);
